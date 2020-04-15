2e949941679e953acef77879e9894123a69f47cb
XANDRO <XANDRO@DESKTOP-U91VMAF>
2020-04-15 02:26:47 +0200

    To PC

--- SistemMenjacnice/Services/ExchangeService.cs
+++ SistemMenjacnice/Services/ExchangeService.cs
@@ -35,42 +35,6 @@ namespace SistemMenjacnice.Services
         /// </summary>
         public void ExchangeMenu()
         {
-            //Valuta valutaEuro = new Valuta("Euro", 117.0000, 118.5000);
-            //Valuta valutaUSD = new Valuta("USD", 102.2000, 109.3500);
-            //Valuta valutaCHF = new Valuta("CHF", 105.1100, 112.7000);
-            //Valuta valutaAUD = new Valuta("AUD", 64.8000, 69.2500);
-            //Valuta valutaCAD = new Valuta("CAD", 73.2000, 78.2500);
-            //Valuta valutaHRK = new Valuta("HRK", 14.1900, 15.8000);
-            //Valuta valutaDKK = new Valuta("DKK", 14.9700, 15.8000);
-            //Valuta valutaHUF = new Valuta("HUF", 0.3060, 0.3340);
-            //Valuta valutaNOK = new Valuta("NOK", 9.9600, 10.5000);
-            //Valuta valutaSEK = new Valuta("SEK", 10.2700, 10.8500);
-            //Valuta valutaGBP = new Valuta("GBP", 129.3500, 133.9897);
-            //Valuta valutaBAM = new Valuta("BAM", 56.5500, 61.5500);
-            //Valuta valutaRUB = new Valuta("RUB", 1.3100, 1.5600);
-            //Valuta valutaCNY = new Valuta("CNY", 14.4400, 16.1100);
-            //Valuta valutaJPY = new Valuta("JPY", 0.9423, 1.0414);
-            //Valuta valutaPLN = new Valuta("PLN", 24.7900, 26.4600);
-            //Valuta valutaCZK = new Valuta("CZK", 4.0100, 4.4500);
-
-            //listaValuta.Add(valutaEuro);
-            //listaValuta.Add(valutaUSD);
-            //listaValuta.Add(valutaCHF);
-            //listaValuta.Add(valutaAUD);
-            //listaValuta.Add(valutaCAD);
-            //listaValuta.Add(valutaHRK);
-            //listaValuta.Add(valutaDKK);
-            //listaValuta.Add(valutaHUF);
-            //listaValuta.Add(valutaNOK);
-            //listaValuta.Add(valutaSEK);
-            //listaValuta.Add(valutaGBP);
-            //listaValuta.Add(valutaBAM);
-            //listaValuta.Add(valutaRUB);
-            //listaValuta.Add(valutaCNY);
-            //listaValuta.Add(valutaJPY);
-            //listaValuta.Add(valutaPLN);
-            //listaValuta.Add(valutaCZK);
-
             listaValuta = new List<Valuta>
             {
                 new Valuta("Euro", 117.0000, 118.5000),
@@ -157,7 +121,7 @@ namespace SistemMenjacnice.Services
         {
             foreach (Valuta valuta in listaValuta)
             {
-                Console.WriteLine(valuta.ID + " " + valuta.Naziv + " " + "{0:0.0000}" + " {1:0.0000}" + " {2:0.0000}", valuta.Prodajni, valuta.Srednji, valuta.Kupovni);
+                Console.WriteLine(valuta.ID.ToString().PadRight(10) + valuta.Naziv.PadRight(10) + "{0:0.0000}" + "{1:0.0000}",valuta.Prodajni,valuta.Srednji);
             }
         }
 
@@ -168,7 +132,7 @@ namespace SistemMenjacnice.Services
         {
             foreach (KursnaLista kursnaLista in kursnaLista)
             {
-                Console.WriteLine(kursnaLista.ID.ToString() + " " + kursnaLista.DatumFormiranja.ToString());
+                Console.WriteLine(kursnaLista.ID.ToString() + " " + kursnaLista.DatumFormiranja.ToString("dd/MM/yyyy"));
             }
 
             Console.WriteLine("=================================");
